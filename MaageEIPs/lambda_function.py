f69cf30d12d097617be12d22416517f769bec153
EC2 Default User <ec2-user@ip-172-31-95-150.ec2.internal>
2023-10-25 18:58:34 +0000

    for MaageEIPs

---
+++ MaageEIPs/lambda_function.py
@@ -0,0 +1,16 @@
+import boto3
+
+def lambda_handler(event, context):
+    ec2_resource=boto3.resource('ec2')
+    
+    
+    for elastic_ip in ec2_resource.vpc_addresses.all():
+        if elastic_ip.instance_id is None:
+            print(f"\n this elastic IP : {elastic_ip} is not associted. releaseing now.. \n")
+            elastic_ip.release()
+            
+  
+    return {
+        'statusCode': 200,
+        'body': ('Processed elastic IP!')
+    }
